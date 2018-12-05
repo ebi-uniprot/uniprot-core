23b57fecc1aba4c54bf3c4e57deb4b51edf460f3
jluo <jluo@ebi.ac.uk>
2018-12-05 11:09:59 +0000

    refactoring cc line converter tests

---
+++ ff-parser/src/test/java/uk/ac/ebi/uniprot/parser/converter/CCWebResourceConverterTest.java
@@ -0,0 +1,48 @@
+package uk.ac.ebi.uniprot.parser.converter;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import uk.ac.ebi.uniprot.domain.uniprot.comment.Comment;
+import uk.ac.ebi.uniprot.domain.uniprot.comment.CommentType;
+import uk.ac.ebi.uniprot.domain.uniprot.comment.WebResourceComment;
+import uk.ac.ebi.uniprot.parser.impl.cc.CcLineConverter;
+import uk.ac.ebi.uniprot.parser.impl.cc.CcLineObject;
+
+public class CCWebResourceConverterTest {
+	private final CcLineConverter converter = new CcLineConverter();
+	
+	@Test
+	public void testWebResource(){
+		//CC   -!- WEB RESOURCE: Name=CD40Lbase; Note=CD40L defect database;
+        //CC       URL="http://bioinf.uta.fi/CD40Lbase/";
+		CcLineObject ccLineO = new CcLineObject();	
+		CcLineObject.CC cc1 =new CcLineObject.CC();
+		cc1.topic =CcLineObject.CCTopicEnum.WEB_RESOURCE;
+		CcLineObject.WebResource wr =new CcLineObject.WebResource();
+		wr.name ="CD40Lbase";
+		wr.note ="CD40L defect database";
+		wr.url ="http://bioinf.uta.fi/CD40Lbase/";
+		cc1.object =wr;
+		ccLineO.ccs.add(cc1);
+	
+		List<Comment> comments = converter.convert(ccLineO) ;
+		assertEquals(1, comments.size());
+		
+		Comment comment1 =comments.get(0);
+		assertEquals(CommentType.WEBRESOURCE, comment1.getCommentType());
+		assertTrue (comment1 instanceof WebResourceComment);
+		
+		WebResourceComment wcomment = (WebResourceComment) comment1;
+		assertEquals("CD40Lbase", wcomment.getResourceName());
+		assertEquals("CD40L defect database", wcomment.getNote());
+		assertEquals("http://bioinf.uta.fi/CD40Lbase/", wcomment.getResourceUrl());
+		assertFalse(wcomment.isFtp());
+		
+	}
+}
