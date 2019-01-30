3afb2413f11fc7bf684a9f3e796a220c8c3fbccc
Edward Turner <eddturner@ebi.ac.uk>
2019-01-30 12:35:02 +0000

    update xmlparser tests to fit with builders

--- xml-parser/src/test/java/uk/ac/ebi/uniprot/xmlparser/uniprot/citation/SubmissionConverterTest.java
+++ xml-parser/src/test/java/uk/ac/ebi/uniprot/xmlparser/uniprot/citation/SubmissionConverterTest.java
@@ -1,49 +1,45 @@
 package uk.ac.ebi.uniprot.xmlparser.uniprot.citation;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.Arrays;
-
 import org.junit.jupiter.api.Test;
-
 import uk.ac.ebi.uniprot.domain.citation.Citation;
 import uk.ac.ebi.uniprot.domain.citation.Submission;
 import uk.ac.ebi.uniprot.domain.citation.SubmissionDatabase;
-import uk.ac.ebi.uniprot.domain.citation.builder.AbstractCitationBuilder;
 import uk.ac.ebi.uniprot.domain.citation.builder.SubmissionBuilder;
 import uk.ac.ebi.uniprot.xml.jaxb.uniprot.CitationType;
 import uk.ac.ebi.uniprot.xmlparser.uniprot.UniProtXmlTestHelper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class SubmissionConverterTest {
 
-	@Test
-	void test() {
-		Submission citation = create();
-		SubmissionConverter converter = new SubmissionConverter();
-		CitationType xmlCitation = converter.toXml(citation);
-		System.out.println(UniProtXmlTestHelper.toXmlString(xmlCitation, CitationType.class, "citation"));
-		Submission converted = converter.fromXml(xmlCitation);
-		assertEquals(citation, converted);
-	}
-	@Test
-	void testCitationConverter() {
-	
-		Citation citation = create();
-		CitationConverter converter = new CitationConverter();
-		CitationType xmlCitation = converter.toXml(citation);
-		System.out.println(UniProtXmlTestHelper.toXmlString(xmlCitation, CitationType.class, "citation"));
-		Citation converted = converter.fromXml(xmlCitation);
-		assertEquals(citation, converted);
-	}
-	
-	private Submission create() {
-		SubmissionBuilder builder = SubmissionBuilder.newInstance();
-		String title = "Protein research";
-		String date = "JAN-2018";
-		builder.submittedToDatabase(SubmissionDatabase.EMBL_GENBANK_DDBJ).title(title)
-				.authors(Arrays.asList(AbstractCitationBuilder.createAuthor("Sulson J.E."),
-						AbstractCitationBuilder.createAuthor("JWaterston R.")))
-				.publicationDate(AbstractCitationBuilder.createPublicationDate(date));
-		return builder.build();
-	}
+    @Test
+    void test() {
+        Submission citation = create();
+        SubmissionConverter converter = new SubmissionConverter();
+        CitationType xmlCitation = converter.toXml(citation);
+        System.out.println(UniProtXmlTestHelper.toXmlString(xmlCitation, CitationType.class, "citation"));
+        Submission converted = converter.fromXml(xmlCitation);
+        assertEquals(citation, converted);
+    }
+
+    @Test
+    void testCitationConverter() {
+        Citation citation = create();
+        CitationConverter converter = new CitationConverter();
+        CitationType xmlCitation = converter.toXml(citation);
+        System.out.println(UniProtXmlTestHelper.toXmlString(xmlCitation, CitationType.class, "citation"));
+        Citation converted = converter.fromXml(xmlCitation);
+        assertEquals(citation, converted);
+    }
+
+    private Submission create() {
+        SubmissionBuilder builder = new SubmissionBuilder();
+        String title = "Protein research";
+        String date = "JAN-2018";
+        builder.submittedToDatabase(SubmissionDatabase.EMBL_GENBANK_DDBJ).title(title)
+                .addAuthor("Sulson J.E.")
+                .addAuthor("JWaterston R.")
+                .publicationDate(date);
+        return builder.build();
+    }
 }
