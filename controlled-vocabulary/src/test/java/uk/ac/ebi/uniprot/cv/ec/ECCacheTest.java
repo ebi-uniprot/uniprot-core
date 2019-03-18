3bf58dfe4069330c516af83ecf330b926dc43b24
Edward Turner <eddturner@ebi.ac.uk>
2019-03-18 15:47:41 +0000

    add EC reader tests

---
+++ controlled-vocabulary/src/test/java/uk/ac/ebi/uniprot/cv/ec/ECCacheTest.java
@@ -0,0 +1,37 @@
+package uk.ac.ebi.uniprot.cv.ec;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.Is.is;
+import static uk.ac.ebi.uniprot.cv.ec.ECCache.FTP_LOCATION;
+
+/**
+ * Created 18/03/19
+ *
+ * @author Edd
+ */
+class ECCacheTest {
+    @Test
+    void canLoadCacheFromFiles() {
+        List<EC> ecs = ECCache.INSTANCE.get("ec/");
+        assertThat(ecs, hasSize(12));
+    }
+
+    @Test
+    void givenWrongFilePath_whenLoadCache_loadsCacheFromFTP() {
+        List<EC> ecs = ECCache.INSTANCE.get("/this/is/wrong/");
+        assertThat(ecs, is(not(emptyList())));
+    }
+
+    @Test
+    void canLoadCacheFromFTP() {
+        List<EC> ecs = ECCache.INSTANCE.get(FTP_LOCATION);
+        assertThat(ecs, is(not(emptyList())));
+    }
+}
