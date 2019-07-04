a0d3dfda04bfe33b7b765fa46817e3f87280777b
jluo <jluo@ebi.ac.uk>
2019-07-04 10:55:36 +0100

    make evidenceline serializable

--- core-domain/src/main/java/uk/ac/ebi/uniprot/domain/uniprot/evidence/EvidenceLine.java
+++ core-domain/src/main/java/uk/ac/ebi/uniprot/domain/uniprot/evidence/EvidenceLine.java
@@ -1,8 +1,9 @@
 package uk.ac.ebi.uniprot.domain.uniprot.evidence;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public interface EvidenceLine {
+public interface EvidenceLine  extends Serializable{
     String getEvidence();
 
     LocalDate getCreateDate();
