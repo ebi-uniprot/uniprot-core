57e5d5f462d9cf2843ae15c7729f51bcec16c300
Shadab Ahmad <sahmad@ebi.ac.uk>
2019-07-17 14:49:29 +0100

    Update the cross ref service to use BasicSearchController and BaseService Classes

---
+++ json-parser/src/main/java/uk/ac/ebi/uniprot/json/parser/crossref/CrossRefJsonConfig.java
@@ -0,0 +1,58 @@
+package uk.ac.ebi.uniprot.json.parser.crossref;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import uk.ac.ebi.uniprot.cv.disease.Disease;
+import uk.ac.ebi.uniprot.cv.disease.impl.DiseaseImpl;
+import uk.ac.ebi.uniprot.cv.keyword.Keyword;
+import uk.ac.ebi.uniprot.cv.keyword.impl.KeywordImpl;
+import uk.ac.ebi.uniprot.domain.crossref.CrossRefEntry;
+import uk.ac.ebi.uniprot.domain.crossref.CrossRefEntryImpl;
+import uk.ac.ebi.uniprot.json.parser.JsonConfig;
+
+public class CrossRefJsonConfig extends JsonConfig {
+    private static CrossRefJsonConfig INSTANCE;
+
+    private final ObjectMapper objectMapper;
+    private final ObjectMapper simpleMapper; // mapper without unwanted fields like type, lighter version
+
+    private CrossRefJsonConfig() {
+        this.objectMapper = initFullObjectMapper();
+        this.simpleMapper = initSimpleObjectMapper();
+    }
+
+    public static synchronized CrossRefJsonConfig getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new CrossRefJsonConfig();
+        }
+        return INSTANCE;
+    }
+
+    @Override
+    public ObjectMapper getSimpleObjectMapper() {
+        return this.simpleMapper;
+    }
+
+    @Override
+    public ObjectMapper getFullObjectMapper() {
+        return this.objectMapper;
+    }
+
+
+    private ObjectMapper initFullObjectMapper() {
+        ObjectMapper objMapper = getDefaultFullObjectMapper();
+
+        // customise the default mapper
+        SimpleModule mod = new SimpleModule();
+        mod.addAbstractTypeMapping(CrossRefEntry.class, CrossRefEntryImpl.class);
+
+        objMapper.registerModule(mod);
+
+        return objMapper;
+    }
+
+    private ObjectMapper initSimpleObjectMapper() {
+        ObjectMapper simpleObjMapper = getDefaultSimpleObjectMapper();
+        return simpleObjMapper;
+    }
+}
