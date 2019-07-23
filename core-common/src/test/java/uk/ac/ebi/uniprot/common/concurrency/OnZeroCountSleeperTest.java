0731a6fa4b47f8f4709ea691c0f470d701b2fd5a
Edward Turner <eddturner@ebi.ac.uk>
2019-07-23 11:25:32 +0100

    refactored already used classes into common, so they can be reused

---
+++ core-common/src/test/java/uk/ac/ebi/uniprot/common/concurrency/OnZeroCountSleeperTest.java
@@ -0,0 +1,139 @@
+package uk.ac.ebi.uniprot.common.concurrency;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created 13/07/19
+ *
+ * @author Edd
+ */
+class OnZeroCountSleeperTest {
+    private static final Logger LOGGER = getLogger(OnZeroCountSleeperTest.class);
+
+    private OnZeroCountSleeper sleeper;
+
+    @BeforeEach
+    void setUp() {
+        this.sleeper = new OnZeroCountSleeper();
+    }
+
+    @Test
+    void canCreateDefaultSleeper() {
+        assertThat(sleeper.getCount(), is(0));
+    }
+
+    @Test
+    void canCreateSleeperWithInitialCount() {
+        int initialCount = 6;
+        OnZeroCountSleeper onZeroCountSleeper = new OnZeroCountSleeper(initialCount, 1000);
+        assertThat(onZeroCountSleeper.getCount(), is(initialCount));
+    }
+
+    @Test
+    void canIncrementSleeper() {
+        sleeper.increment();
+        assertThat(sleeper.getCount(), is(1));
+    }
+
+    @Test
+    void canAddToSleeper() {
+        int delta = 8;
+        sleeper.add(delta);
+        assertThat(sleeper.getCount(), is(delta));
+    }
+
+    @Test
+    void canDecrementSleeper() {
+        int delta = 8;
+        sleeper.add(delta);
+        sleeper.decrement();
+        assertThat(sleeper.getCount(), is(delta - 1));
+    }
+
+    @Test
+    void canMinusFromSleeper() {
+        int plusDelta = 8;
+        int minusDelta = 5;
+        sleeper.add(plusDelta);
+        sleeper.minus(minusDelta);
+        assertThat(sleeper.getCount(), is(plusDelta - minusDelta));
+    }
+
+    @Test
+    void willWaitUntilZero() {
+        OnZeroCountSleeper sleeper = new OnZeroCountSleeper();
+        List<Thread> incrementers = Stream
+                .generate(() -> new Thread(() -> {
+                    int count = sleeper.increment();
+                    LOGGER.info("incremented, so that count = {}", count);
+                }))
+                .limit(5)
+                .collect(toList());
+
+        List<Thread> decrementers = Stream
+                .generate(() -> new Thread(() -> {
+                    try {
+                        Thread.sleep((long) (Math.random() * 10000));
+                    } catch (InterruptedException e) {
+                        // do nothing
+                    }
+                    int count = sleeper.decrement();
+                    LOGGER.info("decremented, so that count = {}", count);
+                }))
+                .limit(5)
+                .collect(toList());
+
+        incrementers.forEach(Thread::start);
+
+        decrementers.forEach(Thread::start);
+
+        sleeper.sleepUntilZero();
+
+        assertThat(sleeper.getCount(), is(0));
+    }
+
+    @Test
+    void willWaitOnlyUntilMaxTimeout() {
+        OnZeroCountSleeper sleeper = new OnZeroCountSleeper(0, 1000);
+        List<Thread> incrementers = Stream
+                .generate(() -> new Thread(() -> {
+                    int count = sleeper.increment();
+                    LOGGER.info("incremented, so that count = {}", count);
+                }))
+                .limit(5)
+                .collect(toList());
+
+        List<Thread> decrementers = Stream
+                .generate(() -> new Thread(() -> {
+                    try {
+                        Thread.sleep((long) (100000));
+                    } catch (InterruptedException e) {
+                        // do nothing
+                    }
+                    int count = sleeper.decrement();
+                    LOGGER.info("decremented, so that count = {}", count);
+                }))
+                .limit(5)
+                .collect(toList());
+
+        incrementers.forEach(Thread::start);
+
+        decrementers.forEach(Thread::start);
+
+        sleeper.sleepUntilZero();
+
+        assertThat(sleeper, is(notNullValue()));
+    }
+}
---
+++ core-common/src/test/java/uk/ac/ebi/uniprot/common/concurrency/TaskExecutorPropertiesTest.java
@@ -0,0 +1,81 @@
+package uk.ac.ebi.uniprot.common.concurrency;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static uk.ac.ebi.uniprot.common.concurrency.TaskExecutorProperties.*;
+import static uk.ac.ebi.uniprot.common.concurrency.TaskExecutorProperties.Builder.createTaskExecutorPropertiesBuilder;
+
+/**
+ * Created 23/07/19
+ *
+ * @author Edd
+ */
+class TaskExecutorPropertiesTest {
+    @Test
+    void canCreateBuilder() {
+        assertThat(createTaskExecutorPropertiesBuilder(), is(notNullValue()));
+    }
+
+    @Test
+    void canSetCorePoolSize() {
+        int value = 5;
+        assertThat(createTaskExecutorPropertiesBuilder().corePoolSize(value).build().getCorePoolSize(), is(value));
+    }
+
+    @Test
+    void canSetMaxPoolSize() {
+        int value = 5;
+        assertThat(createTaskExecutorPropertiesBuilder().maxPoolSize(value).build().getMaxPoolSize(), is(value));
+    }
+
+    @Test
+    void canSetQueueCapacity() {
+        int value = 5;
+        assertThat(createTaskExecutorPropertiesBuilder().queueCapacity(value).build().getQueueCapacity(), is(value));
+    }
+
+    @Test
+    void canSetKeepAlive() {
+        int value = 5;
+        assertThat(createTaskExecutorPropertiesBuilder().keepAliveSeconds(value).build().getKeepAliveSeconds(),
+                   is(value));
+    }
+
+    @Test
+    void canSetAllowCoreThreadTimeout() {
+        boolean value = false;
+        assertThat(createTaskExecutorPropertiesBuilder().allowCoreThreadTimeout(value).build()
+                           .isAllowCoreThreadTimeout(),
+                   is(value));
+    }
+
+    @Test
+    void canSetWaitForCompletion() {
+        boolean value = true;
+        assertThat(createTaskExecutorPropertiesBuilder().waitForTasksToCompleteOnShutdown(value).build()
+                           .isWaitForTasksToCompleteOnShutdown(),
+                   is(value));
+    }
+
+    @Test
+    void canSetThreadNamePrefix() {
+        String value = "lovely-day";
+        assertThat(createTaskExecutorPropertiesBuilder().threadNamePrefix(value).build()
+                           .getThreadNamePrefix(), is(value));
+    }
+
+    @Test
+    void hasCorrectDefaults() {
+        TaskExecutorProperties props = createTaskExecutorPropertiesBuilder().build();
+        assertThat(props.getCorePoolSize(), is(DEFAULT_CORE_POOL_SIZE));
+        assertThat(props.getMaxPoolSize(), is(MAX_POOL_SIZE));
+        assertThat(props.getQueueCapacity(), is(QUEUE_CAPACITY));
+        assertThat(props.getKeepAliveSeconds(), is(KEEP_ALIVE_SECONDS));
+        assertThat(props.isAllowCoreThreadTimeout(), is(ALLOW_CORE_THREAD_TIMEOUT));
+        assertThat(props.isWaitForTasksToCompleteOnShutdown(), is(WAIT_FOR_TASKS_TO_COMPLETE_ON_SHUTDOWN));
+        assertThat(props.getThreadNamePrefix(), is(THREAD_NAME_PREFIX));
+    }
+}
