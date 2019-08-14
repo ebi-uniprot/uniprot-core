922d15db7534ab0b0664ea38f69e3e59a4038657
jluo <jluo@ebi.ac.uk>
2019-08-14 14:20:17 +0100

    add identity to UniRefType

--- core-domain/src/main/java/org/uniprot/core/uniref/UniRefType.java
+++ core-domain/src/main/java/org/uniprot/core/uniref/UniRefType.java
@@ -10,13 +10,20 @@ import org.uniprot.core.util.EnumDisplay;
 */
 
 public enum UniRefType implements EnumDisplay<UniRefType>{
-	UniRef100,
-	UniRef90,
-	UniRef50;
-
+	UniRef100("1.0"),
+	UniRef90("0.9"),
+	UniRef50("0.5");
+	private String identity;
+	
+	UniRefType(String identity){
+		this.identity = identity;
+	}
 	@Override
 	public String toDisplayName() {
 		return name();
+	}
+	public String getIdentity() {
+		return this.identity;
 	}
 }
 
