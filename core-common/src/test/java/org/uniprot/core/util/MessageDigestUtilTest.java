98aa2b0c132d30cbe73d400f2fbf15d4466c79c5
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-09-25 10:53:59 +0100

    REMOVES UNUSED CODE

--- core-common/src/test/java/org/uniprot/core/util/MessageDigestUtilTest.java
+++ core-common/src/test/java/org/uniprot/core/util/MessageDigestUtilTest.java
@@ -11,19 +11,5 @@ class MessageDigestUtilTest {
     @Test
     void testGetDigest() {
         assertEquals("900150983CD24FB0D6963F7D28E17F72", MessageDigestUtil.getDigest("abc", "MD5"));
-
     }
-
-    @Test
-    void testMD5() {
-        assertEquals("900150983CD24FB0D6963F7D28E17F72", MessageDigestUtil.getMD5("abc"));
-
-    }
-
-    @Test
-    void testGetDigestFromBytes() {
-        assertEquals("900150983CD24FB0D6963F7D28E17F72", MessageDigestUtil.getDigest("abc".getBytes(), "MD5"));
-
-    }
-
 }
