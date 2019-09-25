8fce9b82bac4de274677a17d871278c8066144de
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-09-25 12:03:59 +0100

    IMPROVES TESTS

--- core-common/src/test/java/org/uniprot/core/util/MessageDigestUtilTest.java
+++ core-common/src/test/java/org/uniprot/core/util/MessageDigestUtilTest.java
@@ -8,8 +8,13 @@ import org.uniprot.core.util.MessageDigestUtil;
 
 class MessageDigestUtilTest {
 
-    @Test
-    void testGetDigest() {
-        assertEquals("900150983CD24FB0D6963F7D28E17F72", MessageDigestUtil.getDigest("abc", "MD5"));
-    }
+  @Test
+  void testGetMd5Digest() {
+    assertEquals("900150983CD24FB0D6963F7D28E17F72", MessageDigestUtil.getMD5("abc"));
+  }
+
+  @Test
+  void forWrongTypeGetDigest_returnSameString() {
+    assertEquals("abc", MessageDigestUtil.getDigest("abc", "abc"));
+  }
 }
