c96a88da064c47d96ae570a8bd178553f74e4b17
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-09-25 13:55:17 +0100

    ADDS TESTS

--- core-common/src/test/java/org/uniprot/core/util/UtilsTest.java
+++ core-common/src/test/java/org/uniprot/core/util/UtilsTest.java
@@ -9,22 +9,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 class UtilsTest {
 
-  @Test
-  void testCapitalize() {
-    String str = "protein";
-    String result = Utils.upperFirstChar(str);
-    assertEquals("Protein", result);
-
-  }
-
-  @Test
-  void testUncapitalize() {
-    String str = "Protein";
-    String result = Utils.lowerFirstChar(str);
-    assertEquals("protein", result);
-
-  }
-
   @Nested
   class Strings {
     @Nested
@@ -43,23 +27,75 @@ class UtilsTest {
     }
 
     @Nested
-		class nullOrEmpty {
-			@Test
-			void whenNull_returnTrue() {
-				assertTrue(Utils.nullOrEmpty((String)null));
-			}
+    class nullOrEmpty {
+      @Test
+      void whenNull_returnTrue() {
+        assertTrue(Utils.nullOrEmpty((String) null));
+      }
 
-			@Test
-			void whenEmpty_returnTrue() {
-				assertTrue(Utils.nullOrEmpty(""));
-			}
+      @Test
+      void whenEmpty_returnTrue() {
+        assertTrue(Utils.nullOrEmpty(""));
+      }
 
-			@ParameterizedTest
-			@ValueSource(strings = {"1", "AB", "a%^b", "()+^£$^%%", "aB", "Ab", " s ", "NULL", "   ", "123432"})
-			void nonEmpty_shouldAlwaysReturnFalse(String test) {
-				assertEquals(test, Utils.nullToEmpty(test));
-			}
-		}
+      @ParameterizedTest
+      @ValueSource(strings = {"1", "AB", "a%^b", "()+^£$^%%", "aB", "Ab", " s ", "NULL", "   ", "123432"})
+      void nonEmpty_shouldAlwaysReturnFalse(String test) {
+        assertEquals(test, Utils.nullToEmpty(test));
+      }
+    }
+
+    @Nested
+    class upperFirstChar {
+      @Test
+      void whenNullOrEmpty_returnSame() {
+        assertAll(
+          () -> assertNull(Utils.upperFirstChar(null)),
+          () -> assertEquals("", Utils.upperFirstChar(""))
+        );
+      }
+
+      @Test
+      void capitalizeFirstChar() {
+        assertAll(
+          () -> assertEquals("Protein", Utils.upperFirstChar("protein")),
+          () -> assertEquals(" protein", Utils.upperFirstChar(" protein")),
+          () -> assertEquals("Protein abc", Utils.upperFirstChar("protein abc")),
+          () -> assertEquals("$protein", Utils.upperFirstChar("$protein")),
+          () -> assertEquals("P!rotein", Utils.upperFirstChar("p!rotein")),
+          () -> assertEquals("Protein Def", Utils.upperFirstChar("protein Def")),
+          () -> assertEquals("12345", Utils.upperFirstChar("12345"))
+        );
+      }
+    }
+
+    @Nested
+    class lowerFirstChar {
+      @Test
+      void whenNullOrEmpty_returnSame() {
+        assertAll(
+          () -> assertNull(Utils.lowerFirstChar(null)),
+          () -> assertEquals("", Utils.lowerFirstChar(""))
+        );
+      }
+
+      @Test
+      void uncapitalizeFirstChar() {
+        String str = "Protein";
+        String result = Utils.lowerFirstChar(str);
+        assertEquals("protein", result);
+        assertAll(
+          () -> assertEquals("protein", Utils.lowerFirstChar("Protein")),
+          () -> assertEquals(" protein", Utils.lowerFirstChar(" protein")),
+          () -> assertEquals("protein abc", Utils.lowerFirstChar("Protein abc")),
+          () -> assertEquals("$protein", Utils.lowerFirstChar("$protein")),
+          () -> assertEquals("p!rotein", Utils.lowerFirstChar("P!rotein")),
+          () -> assertEquals("protein Def", Utils.lowerFirstChar("Protein Def")),
+          () -> assertEquals("12345", Utils.lowerFirstChar("12345"))
+        );
+      }
+    }
+
   }
 }
 
