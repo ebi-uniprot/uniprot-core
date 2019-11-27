8b7179cd2b543ce4f461a383d678c938bbe61cd7
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-11-27 13:57:15 +0000

    ANNOTATES AND ADDS MISSING TESTS

--- core-domain/src/main/java/org/uniprot/core/citation/builder/ElectronicArticleBuilder.java
+++ core-domain/src/main/java/org/uniprot/core/citation/builder/ElectronicArticleBuilder.java
@@ -3,12 +3,14 @@ package org.uniprot.core.citation.builder;
 import org.uniprot.core.citation.ElectronicArticle;
 import org.uniprot.core.citation.impl.ElectronicArticleImpl;
 
+import javax.annotation.Nonnull;
+
 public final class ElectronicArticleBuilder
         extends AbstractCitationBuilder<ElectronicArticleBuilder, ElectronicArticle> {
     private String journalName;
     private String locator;
 
-    public ElectronicArticle build() {
+    public @Nonnull ElectronicArticle build() {
         return new ElectronicArticleImpl(
                 authoringGroups,
                 authors,
@@ -20,18 +22,18 @@ public final class ElectronicArticleBuilder
     }
 
     @Override
-    public ElectronicArticleBuilder from(ElectronicArticle instance) {
+    public @Nonnull ElectronicArticleBuilder from(@Nonnull ElectronicArticle instance) {
         init(instance);
         return this.journalName(instance.getJournal().getName())
                 .locator(instance.getLocator().getValue());
     }
 
-    public ElectronicArticleBuilder journalName(String journalName) {
+    public @Nonnull ElectronicArticleBuilder journalName(String journalName) {
         this.journalName = journalName;
         return this;
     }
 
-    public ElectronicArticleBuilder locator(String locator) {
+    public @Nonnull ElectronicArticleBuilder locator(String locator) {
         this.locator = locator;
         return this;
     }
