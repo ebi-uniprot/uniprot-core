3ee2c09135734c1fa6a0f55fe0863a4233a4d70c
Rizwan Ishtiaq <rizwan.ishtiaq@hotmail.com>
2019-11-28 11:47:31 +0000

    ANNOTATES AND ADDS MISSING TESTS

--- core-domain/src/main/java/org/uniprot/core/citation/builder/UnpublishedBuilder.java
+++ core-domain/src/main/java/org/uniprot/core/citation/builder/UnpublishedBuilder.java
@@ -3,6 +3,8 @@ package org.uniprot.core.citation.builder;
 import org.uniprot.core.citation.Unpublished;
 import org.uniprot.core.citation.impl.UnpublishedImpl;
 
+import javax.annotation.Nonnull;
+
 public final class UnpublishedBuilder
         extends AbstractCitationBuilder<UnpublishedBuilder, Unpublished> {
     public Unpublished build() {
@@ -10,7 +12,7 @@ public final class UnpublishedBuilder
     }
 
     @Override
-    public UnpublishedBuilder from(Unpublished instance) {
+    public @Nonnull UnpublishedBuilder from(Unpublished instance) {
         init(instance);
         return this;
     }
