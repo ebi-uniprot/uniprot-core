4ca409d83ad309d0f87b7a20b8f1f0d7302bbaa6
jluo <jluo@ebi.ac.uk>
2020-03-24 10:08:12 +0000

    fix the testcases

--- tools-entry-scorer/src/test/java/org/uniprot/core/scorer/uniprotkb/comments/InteractionScoredTest.java
+++ tools-entry-scorer/src/test/java/org/uniprot/core/scorer/uniprotkb/comments/InteractionScoredTest.java
@@ -8,9 +8,9 @@ class InteractionScoredTest extends CommentScoreTestBase {
     void shouldScore90() throws Exception {
         String line =
                 "CC   -!- INTERACTION:\n"
-                        + "CC       Self; NbExp=1; IntAct=EBI-2856, EBI-2856;\n"
-                        + "CC       Q02821:SRP1; NbExp=2; IntAct=EBI-2856, EBI-1797;\n"
-                        + "CC       P84198:VIM (xeno); NbExp=4; IntAct=EBI-356498, EBI-457639;";
+                        + "CC       P12345; P12345-1; NbExp=1; IntAct=EBI-2856, EBI-2856;\n"
+                        + "CC       P12345; Q02821: SRP1; NbExp=2; IntAct=EBI-2856, EBI-1797;\n"
+                        + "CC       P12345; P84198: VIM; Xeno; NbExp=4; IntAct=EBI-356498, EBI-457639;";
         verify(CommentType.INTERACTION, line, 9.0);
     }
 }
