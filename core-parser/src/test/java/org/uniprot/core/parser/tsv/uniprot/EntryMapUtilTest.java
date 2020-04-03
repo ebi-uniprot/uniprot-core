a44ccc1d3caa1e44592fbb331fe21452c1009e6f
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-04-03 09:24:20 +0100

    add test cases

---
+++ core-parser/src/test/java/org/uniprot/core/parser/tsv/uniprot/EntryMapUtilTest.java
@@ -0,0 +1,35 @@
+package org.uniprot.core.parser.tsv.uniprot;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class EntryMapUtilTest {
+
+    @Test
+    void testConvertMapToTSVString() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("open", 2);
+        map.put("yaml", 1);
+        map.put("ada", 12);
+        map.put("Zoo", 100);
+        String tsvString = EntryMapUtil.convertMapToTSVString(map);
+        Assertions.assertEquals("Zoo (100); ada (12); open (2); yaml (1)", tsvString);
+    }
+
+    @Test
+    void testConvertEmptyMapToTSVString() {
+        Map<String, Integer> map = new HashMap<>();
+        String tsvString = EntryMapUtil.convertMapToTSVString(map);
+        Assertions.assertTrue(tsvString.isEmpty());
+    }
+
+    @Test
+    void testConvertNullMapToTSVString() {
+        Map<String, Integer> map = null;
+        String tsvString = EntryMapUtil.convertMapToTSVString(map);
+        Assertions.assertTrue(tsvString.isEmpty());
+    }
+}
