2b79cbe55994e0c4f2afcc66c3a5a4842956a59a
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-04-09 15:46:50 +0100

    minor fix in an implentation

--- core-domain/src/main/java/org/uniprot/core/unirule/impl/PositionalRuleExceptionImpl.java
+++ core-domain/src/main/java/org/uniprot/core/unirule/impl/PositionalRuleExceptionImpl.java
@@ -3,9 +3,9 @@ package org.uniprot.core.unirule.impl;
 import java.util.List;
 
 import org.uniprot.core.uniprotkb.UniProtKBAccession;
+import org.uniprot.core.unirule.PositionalFeature;
 
-public class PositionalRuleExceptionImpl<PositionalFeature>
-        extends AbstractRuleException<PositionalFeature> {
+public class PositionalRuleExceptionImpl extends AbstractRuleException<PositionalFeature> {
 
     private static final long serialVersionUID = -1465183148205445458L;
 
