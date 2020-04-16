7331e5d9dfcae43fdc1332d53a68885b681e8a31
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-04-16 19:28:38 +0100

    none of the rulebuilder field is mandatory

--- core-domain/src/test/java/org/uniprot/core/unirule/impl/CaseRuleBuilderTest.java
+++ core-domain/src/test/java/org/uniprot/core/unirule/impl/CaseRuleBuilderTest.java
@@ -1,13 +1,14 @@
 package org.uniprot.core.unirule.impl;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import org.junit.jupiter.api.Test;
 import org.uniprot.core.unirule.Annotation;
 import org.uniprot.core.unirule.CaseRule;
 import org.uniprot.core.unirule.ConditionSet;
@@ -15,13 +16,93 @@ import org.uniprot.core.unirule.RuleException;
 
 public class CaseRuleBuilderTest {
 
+    @Test
+    void testSetStatsExempted() {
+        CaseRuleBuilder builder = new CaseRuleBuilder();
+        builder.overallStatsExempted(true);
+        CaseRule object = builder.build();
+        assertNotNull(object);
+        assertTrue(object.isOverallStatsExempted());
+    }
+
+    @Test
+    void testConditionSetListAppend() {
+        CaseRule object = createObject();
+        CaseRuleBuilder builder = CaseRuleBuilder.from(object);
+        // append one item to the list
+        ConditionSet conditionSet = ConditionSetBuilderTest.createObject();
+        builder.conditionSetsAdd(conditionSet);
+        CaseRule updatedObject = builder.build();
+        assertNotNull(updatedObject);
+        assertEquals(object.getConditionSets().size() + 1, updatedObject.getConditionSets().size());
+    }
+
+    @Test
+    void testAnnotationListAppend() {
+        CaseRule object = createObject();
+        CaseRuleBuilder builder = CaseRuleBuilder.from(object);
+        // append an item to the list
+        Annotation annotation = AnnotationBuilderTest.createObject();
+        builder.annotationsAdd(annotation);
+        CaseRule updatedObject = builder.build();
+        assertNotNull(updatedObject);
+        assertEquals(object.getAnnotations().size() + 1, updatedObject.getAnnotations().size());
+    }
+
+    @Test
+    void testRuleExceptionListAppend() {
+        CaseRule object = createObject();
+        CaseRuleBuilder builder = CaseRuleBuilder.from(object);
+        // append one item to the list
+        RuleException<Annotation> ruleException = AnnotationRuleExceptionImplTest.createObject();
+        builder.ruleExceptionsAdd(ruleException);
+        CaseRule updatedObject = builder.build();
+        assertNotNull(updatedObject);
+        assertEquals(
+                object.getRuleExceptions().size() + 1, updatedObject.getRuleExceptions().size());
+    }
+
+    @Test
+    void testAddConditionSetList() {
+        CaseRuleBuilder builder = new CaseRuleBuilder();
+        // add one item to the empty list
+        ConditionSet conditionSet = ConditionSetBuilderTest.createObject();
+        builder.conditionSetsAdd(conditionSet);
+        CaseRule object = builder.build();
+        assertNotNull(object);
+        assertEquals(Arrays.asList(conditionSet), object.getConditionSets());
+    }
+
+    @Test
+    void testAddAnnotationList() {
+        CaseRuleBuilder builder = new CaseRuleBuilder();
+        // add one item to the empty list
+        Annotation annotation = AnnotationBuilderTest.createObject();
+        builder.annotationsAdd(annotation);
+        CaseRule object = builder.build();
+        assertNotNull(object);
+        assertEquals(Arrays.asList(annotation), object.getAnnotations());
+    }
+
+    @Test
+    void testAddRuleExceptionList() {
+        CaseRuleBuilder builder = new CaseRuleBuilder();
+        // add one item to the empty list
+        RuleException<Annotation> ruleException = AnnotationRuleExceptionImplTest.createObject();
+        builder.ruleExceptionsAdd(ruleException);
+        CaseRule object = builder.build();
+        assertNotNull(object);
+        assertEquals(Arrays.asList(ruleException), object.getRuleExceptions());
+    }
+
     public static CaseRule createObject() {
         List<ConditionSet> conditionSets = ConditionSetBuilderTest.createObjects(2);
         List<Annotation> annotations = AnnotationBuilderTest.createObjects(3);
         List<RuleException<Annotation>> ruleExceptions =
                 AnnotationRuleExceptionImplTest.createObjects(3);
-        CaseRuleBuilder<Annotation> builder =
-                new CaseRuleBuilder<>(conditionSets, annotations, ruleExceptions);
+        CaseRuleBuilder<Annotation> builder = new CaseRuleBuilder<>();
+        builder.conditionSetsSet(conditionSets).annotationsSet(annotations);
+        builder.ruleExceptionsSet(ruleExceptions);
         boolean exempted = ThreadLocalRandom.current().nextBoolean();
         builder.overallStatsExempted(exempted);
         CaseRule rule = builder.build();
