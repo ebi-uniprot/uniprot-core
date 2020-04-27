c4ce50c4fc5fc08a6bc5152295690c1fe2e5a31c
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-04-27 22:25:54 +0100

    fix conflict

--- core-domain/src/test/java/org/uniprot/core/unirule/impl/FtagConditionValueBuilderTest.java
+++ core-domain/src/test/java/org/uniprot/core/unirule/impl/FtagConditionValueBuilderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -12,7 +13,7 @@ import org.uniprot.core.unirule.FtagConditionValue;
 
 public class FtagConditionValueBuilderTest {
 
-    public static FtagConditionValue createObject() {
+    public static FtagConditionValue createObject(int listSize) {
         String random = UUID.randomUUID().toString();
         String value = "value-" + random;
         String pattern = "pattern-" + random;
@@ -25,7 +26,14 @@ public class FtagConditionValueBuilderTest {
         return tag;
     }
 
+    public static FtagConditionValue createObject() {
+        int listSize = ThreadLocalRandom.current().nextInt(1, 5);
+        return createObject(listSize);
+    }
+
     public static List<FtagConditionValue> createObjects(int count) {
-        return IntStream.range(0, count).mapToObj(i -> createObject()).collect(Collectors.toList());
+        return IntStream.range(0, count)
+                .mapToObj(i -> createObject(count))
+                .collect(Collectors.toList());
     }
 }
