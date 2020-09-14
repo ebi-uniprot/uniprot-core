1149e0f1f09bc7b818d74a1f46b6765de8a57f83
lgonzales <lgonzales@ebi.ac.uk>
2020-09-14 14:02:02 +0100

    rollback UniProtKBCrossReference Serializer and Deserializer

--- json-parser/src/main/java/org/uniprot/core/json/parser/uniprot/deserializer/UniProtKBCrossReferenceDeserializer.java
+++ json-parser/src/main/java/org/uniprot/core/json/parser/uniprot/deserializer/UniProtKBCrossReferenceDeserializer.java
@@ -2,10 +2,8 @@ package org.uniprot.core.json.parser.uniprot.deserializer;
 
 import java.io.IOException;
 
-import org.uniprot.core.cv.xdb.UniProtDatabaseAttribute;
 import org.uniprot.core.json.parser.JsonParserException;
 import org.uniprot.core.uniprotkb.evidence.impl.EvidenceImpl;
-import org.uniprot.core.uniprotkb.xdb.UniProtKBDatabase;
 import org.uniprot.core.uniprotkb.xdb.impl.UniProtCrossReferenceBuilder;
 import org.uniprot.core.uniprotkb.xdb.impl.UniProtKBCrossReferenceImpl;
 import org.uniprot.cv.xdb.UniProtKBDatabaseImpl;
@@ -36,39 +34,39 @@ public class UniProtKBCrossReferenceDeserializer
         UniProtCrossReferenceBuilder builder = new UniProtCrossReferenceBuilder();
         ObjectNode node = jp.readValueAsTree();
         if (node != null) {
-            if (node.has("drLine")) {
-                JsonNode drLine = node.get("drLine");
-                parseDrLine(drLine.asText(), builder);
-            }
-            if (node.has("evidences")) {
-                node.get("evidences")
-                        .elements()
-                        .forEachRemaining(evidence -> parseEvidence(jp,builder, evidence));
-            }
+            node.fields()
+                    .forEachRemaining(
+                            entry -> {
+                                switch (entry.getKey()) {
+                                    case "database":
+                                        String dbValue = entry.getValue().asText();
+                                        builder.database(new UniProtKBDatabaseImpl(dbValue));
+                                        break;
+
+                                    case "id":
+                                        builder.id(entry.getValue().asText());
+                                        break;
+
+                                    case "isoformId":
+                                        builder.isoformId(entry.getValue().asText());
+                                        break;
+
+                                    case "evidences":
+                                        entry.getValue()
+                                                .elements()
+                                                .forEachRemaining(
+                                                        evidence ->
+                                                                parseEvidence(
+                                                                        jp, builder, evidence));
+                                        break;
+
+                                    default:
+                                        builder.propertiesAdd(
+                                                entry.getKey(), entry.getValue().asText());
+                                }
+                            });
         }
         return (UniProtKBCrossReferenceImpl) builder.build();
-    }
-
-    private void parseDrLine(String drLine, UniProtCrossReferenceBuilder builder) {
-        String[] drProps = drLine.split(";");
-        UniProtKBDatabase database = new UniProtKBDatabaseImpl(drProps[0].trim());
-        builder.database(database);
-        builder.id(drProps[1].trim());
-
-        String isoFormId = drProps[2].trim();
-        if (!"-".equals(isoFormId)) {
-            builder.isoformId(isoFormId);
-        }
-
-        for (int i = 3; i < drProps.length; i++) {
-            String propValue = drProps[i].trim();
-            if (!"-".equals(propValue)) {
-                UniProtDatabaseAttribute attribute = database.getUniProtDatabaseAttribute(i - 3);
-                if (attribute != null) {
-                    builder.propertiesAdd(attribute, propValue);
-                }
-            }
-        }
     }
 
     private void parseEvidence(
--- json-parser/src/main/java/org/uniprot/core/json/parser/uniprot/serializer/UniProtKBCrossReferenceSerializer.java
+++ json-parser/src/main/java/org/uniprot/core/json/parser/uniprot/serializer/UniProtKBCrossReferenceSerializer.java
@@ -2,12 +2,8 @@ package org.uniprot.core.json.parser.uniprot.serializer;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 import org.uniprot.core.Property;
-import org.uniprot.core.cv.xdb.UniProtDatabaseAttribute;
-import org.uniprot.core.uniprotkb.xdb.UniProtKBDatabase;
 import org.uniprot.core.uniprotkb.xdb.impl.UniProtKBCrossReferenceImpl;
 
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -32,48 +28,26 @@ public class UniProtKBCrossReferenceSerializer extends StdSerializer<UniProtKBCr
             throws IOException {
         gen.writeStartObject();
 
-        gen.writeStringField("drLine", buildDrLine(crossReference));
-        gen.writeObjectField("evidences", crossReference.getEvidences());
-
-        gen.writeEndObject();
-    }
-
-    private String buildDrLine(UniProtKBCrossReferenceImpl crossReference) {
-        StringBuilder drLine = new StringBuilder();
-
-        UniProtKBDatabase database = crossReference.getDatabase();
-        drLine.append(database.getName()).append(";");
-        drLine.append(crossReference.getId()).append(";");
+        gen.writeStringField("database", crossReference.getDatabase().getName());
+        gen.writeStringField("id", crossReference.getId());
 
         if (crossReference.hasIsoformId()) {
-            drLine.append(crossReference.getIsoformId()).append(";");
-        } else {
-            drLine.append("-;");
+            gen.writeStringField("isoformId", crossReference.getIsoformId());
+        }
+
+        if (crossReference.hasEvidences()) {
+            gen.writeObjectField("evidences", crossReference.getEvidences());
         }
 
         if (crossReference.hasProperties()) {
-            drLine.append(buildProperty(database, crossReference.getProperties()));
-        } else {
-            drLine.append("-");
+            buildProperties(gen, crossReference.getProperties());
         }
-
-        return drLine.toString();
+        gen.writeEndObject();
     }
 
-    private String buildProperty(UniProtKBDatabase database, List<Property> properties) {
-        StringBuilder builder = new StringBuilder();
-        Map<String, String> props =
-                properties.stream().collect(Collectors.toMap(Property::getKey, Property::getValue));
-        int propSize = database.getUniProtDatabaseDetail().getAttributes().size();
-        for (int i = 0; i < propSize; i++) {
-            UniProtDatabaseAttribute attribute = database.getUniProtDatabaseAttribute(i);
-            if (attribute != null) {
-                builder.append(props.getOrDefault(attribute.getName(), "-"));
-                if (i < propSize - 1) {
-                    builder.append(";");
-                }
-            }
+    private void buildProperties(JsonGenerator gen, List<Property> properties) throws IOException {
+        for (Property property : properties) {
+            gen.writeStringField(property.getKey(), property.getValue());
         }
-        return builder.toString();
     }
 }
