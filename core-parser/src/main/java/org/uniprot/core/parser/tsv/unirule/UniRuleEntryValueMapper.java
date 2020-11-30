a3b6ebea173c01d12da0967345497ead1bbcd846
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-11-30 13:49:53 +0000

    address the review comment

--- core-parser/src/main/java/org/uniprot/core/parser/tsv/unirule/UniRuleEntryValueMapper.java
+++ core-parser/src/main/java/org/uniprot/core/parser/tsv/unirule/UniRuleEntryValueMapper.java
@@ -26,7 +26,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -45,9 +44,9 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
         return map;
     }
 
-    private Long getProteinAnnotatedCount(UniRuleEntry uniRuleEntry){
+    private Long getProteinAnnotatedCount(UniRuleEntry uniRuleEntry) {
         Long proteinAnnotatedCount = uniRuleEntry.getProteinsAnnotatedCount();
-        if(Utils.notNull(proteinAnnotatedCount)){
+        if (Utils.notNull(proteinAnnotatedCount)) {
             return proteinAnnotatedCount;
         } else {
             return 0L;
@@ -56,14 +55,19 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
 
     private String getTaxonomicScope(UniRuleEntry uniRuleEntry) {
         Rule mainRule = uniRuleEntry.getMainRule();
-        Optional<Condition> optCondition = mainRule.getConditionSets().stream().map(ConditionSet::getConditions)
-                .flatMap(Collection::stream).filter(condition -> "taxon".equals(condition.getType())).findFirst();
-        if(optCondition.isPresent()){
-            List<ConditionValue> conditionValues = optCondition.get().getConditionValues();
-            if(Utils.notNullNotEmpty(conditionValues)) {
-                List<String> taxonScopes = conditionValues.stream().map(cv -> getTaxonomicScope(cv)).collect(Collectors.toList());
-                return String.join(",", taxonScopes);
-            }
+        return mainRule.getConditionSets().stream().map(ConditionSet::getConditions)
+                .flatMap(Collection::stream).filter(condition -> "taxon".equals(condition.getType())).findFirst()
+                .map(this::getConditionValues).orElse(EMPTY_STRING);
+    }
+
+    private String getConditionValues(Condition condition) {
+        List<ConditionValue> conditionValues = condition.getConditionValues();
+        if (Utils.notNullNotEmpty(conditionValues)) {
+            List<String> taxonScopes =
+                    conditionValues.stream()
+                            .map(this::getTaxonomicScope)
+                            .collect(Collectors.toList());
+            return String.join(",", taxonScopes);
         }
         return EMPTY_STRING;
     }
@@ -77,16 +81,15 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
     private String getUniProtAccessions(Information information) {
         List<UniProtKBAccession> uniProtAccessions = information.getUniProtAccessions();
         if (Utils.notNullNotEmpty(uniProtAccessions)) {
-            List<String> accessions = uniProtAccessions.stream().map(UniProtKBAccession::getValue).collect(Collectors.toList());
-            return String.join(",", accessions);
+            return uniProtAccessions.stream().map(UniProtKBAccession::getValue).collect(Collectors.joining(","));
         } else {
             return EMPTY_STRING;
         }
     }
 
-    private String getAnnotationCovered(UniRuleEntry entry){
+    private String getAnnotationCovered(UniRuleEntry entry) {
         List<Annotation> annotations = entry.getMainRule().getAnnotations();
-        if(Utils.notNullNotEmpty(annotations)){
+        if (Utils.notNullNotEmpty(annotations)) {
             List<String> commentTypes = annotations.stream().map(Annotation::getComment)
                     .map(Comment::getCommentType).map(CommentType::getName).collect(Collectors.toList());
             List<String> dbType = annotations.stream().map(Annotation::getDbReference)
@@ -95,10 +98,10 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
             Gene gene = annotations.stream().map(Annotation::getGene).findFirst().orElse(null);
             Set<String> annotCovered = new HashSet<>(commentTypes);
             annotCovered.addAll(dbType);
-            if(Utils.notNull(keyword)){
+            if (Utils.notNull(keyword)) {
                 annotCovered.add("keyword");
             }
-            if(Utils.notNull(gene)){
+            if (Utils.notNull(gene)) {
                 annotCovered.add("gene");
             }
             return String.join(",", annotCovered);
@@ -110,18 +113,18 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
         // get protein name from main rule
         List<Annotation> annotations = uniRuleEntry.getMainRule().getAnnotations();
         StringBuilder builder = new StringBuilder();
-        if(Utils.notNullNotEmpty(annotations)){
+        if (Utils.notNullNotEmpty(annotations)) {
             builder = getProteinName(annotations);
         }
         // get protein name from case rules
         List<CaseRule> otherRules = uniRuleEntry.getOtherRules();
-        if(Utils.notNullNotEmpty(otherRules)) {
+        if (Utils.notNullNotEmpty(otherRules)) {
             for (CaseRule caseRule : otherRules) {
                 StringBuilder proteinNameBuilder = getProteinName(caseRule.getAnnotations());
-                if(builder.length() > 0){
+                if (builder.length() > 0) {
                     builder.append("|");
                 }
-                if(Utils.notNullNotEmpty(proteinNameBuilder.toString())){
+                if (Utils.notNullNotEmpty(proteinNameBuilder.toString())) {
                     builder.append(proteinNameBuilder);
                 }
             }
@@ -130,32 +133,37 @@ public class UniRuleEntryValueMapper implements EntityValueMapper<UniRuleEntry> 
         return builder.toString();
     }
 
-    private StringBuilder getProteinName(List<Annotation> annotations){
+    private StringBuilder getProteinName(List<Annotation> annotations) {
         ProteinDescription proteinDescription = annotations.stream().map(Annotation::getProteinDescription)
                 .findFirst().orElse(null);
         StringBuilder builder = new StringBuilder();
-        if(Utils.notNull(proteinDescription)){
-            String fullName = proteinDescription.getRecommendedName().getFullName().getValue();
-            List<String> ecNumbers = proteinDescription.getRecommendedName().getEcNumbers().stream()
-                    .map(EC::getValue).filter(val -> Utils.notNullNotEmpty(val))
-                    .map(val -> "EC:" + val).collect(Collectors.toList());
+        if (Utils.notNull(proteinDescription)) {
+            String fullName = null;
+            List<String> ecNumbers = null;
+            List<String> shortNames = null;
+            if (Utils.notNull(proteinDescription.getRecommendedName())) {
+                fullName = proteinDescription.getRecommendedName().getFullName().getValue();
+                ecNumbers = proteinDescription.getRecommendedName().getEcNumbers().stream()
+                        .map(EC::getValue).filter(Utils::notNullNotEmpty)
+                        .map(val -> "EC:" + val).collect(Collectors.toList());
 
-            List<String> shortNames = proteinDescription.getRecommendedName().getShortNames().stream()
-                    .map(Name::getValue).filter(val -> Utils.notNullNotEmpty(val))
-                    .map(val -> "Short:" + val).collect(Collectors.toList());
+                shortNames = proteinDescription.getRecommendedName().getShortNames().stream()
+                        .map(Name::getValue).filter(Utils::notNullNotEmpty)
+                        .map(val -> "Short:" + val).collect(Collectors.toList());
+            }
 
-            if(Utils.notNullNotEmpty(fullName)){
+            if (Utils.notNullNotEmpty(fullName)) {
                 builder.append("Full:").append(fullName);
             }
 
-            if(Utils.notNullNotEmpty(shortNames)){
-                if(builder.length() > 0){
+            if (Utils.notNullNotEmpty(shortNames)) {
+                if (builder.length() > 0) {
                     builder.append("|");
                 }
                 builder.append(String.join("|", shortNames));
             }
-            if(Utils.notNullNotEmpty(ecNumbers)){
-                if(builder.length() > 0){
+            if (Utils.notNullNotEmpty(ecNumbers)) {
+                if (builder.length() > 0) {
                     builder.append("|");
                 }
                 builder.append(String.join("|", ecNumbers));
