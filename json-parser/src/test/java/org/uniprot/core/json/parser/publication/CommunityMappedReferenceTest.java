51d8b09400b927f2ca3c0c23a82441b839e709e6
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-12-10 11:29:34 +0000

    add json config for community and computational annotation

---
+++ json-parser/src/test/java/org/uniprot/core/json/parser/publication/CommunityMappedReferenceTest.java
@@ -0,0 +1,56 @@
+package org.uniprot.core.json.parser.publication;
+
+import org.junit.jupiter.api.Test;
+import org.uniprot.core.json.parser.ValidateJson;
+import org.uniprot.core.publication.CommunityMappedReference;
+import org.uniprot.core.publication.MappedSource;
+import org.uniprot.core.publication.impl.CommunityAnnotationBuilder;
+import org.uniprot.core.publication.impl.CommunityMappedReferenceBuilder;
+import org.uniprot.core.publication.impl.MappedSourceBuilder;
+import org.uniprot.core.uniprotkb.UniProtKBAccession;
+import org.uniprot.core.uniprotkb.impl.UniProtKBAccessionBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author sahmad
+ */
+public class CommunityMappedReferenceTest {
+
+    @Test
+    void testSimpleCommunityMappedReference() {
+        CommunityMappedReferenceBuilder builder = new CommunityMappedReferenceBuilder();
+        CommunityMappedReference communityMappedReference = builder.build();
+        ValidateJson.verifyJsonRoundTripParser(
+                CommunityMappedReferenceJsonConfig.getInstance().getFullObjectMapper(), communityMappedReference);
+    }
+
+    @Test
+    void testCompleteCommunityMappedReference() {
+        CommunityMappedReference communityMappedReference = getCompleteCommunityMMappedReference();
+        ValidateJson.verifyJsonRoundTripParser(
+                CommunityMappedReferenceJsonConfig.getInstance().getFullObjectMapper(), communityMappedReference);
+        ValidateJson.verifyEmptyFields(communityMappedReference);
+    }
+
+    static CommunityMappedReference getCompleteCommunityMMappedReference() {
+        String protOrGene = "protOrGene";
+        String function = "function";
+        String disease = "disease";
+        String comment = "comment";
+        UniProtKBAccession accession = new UniProtKBAccessionBuilder("P12345").build();
+        MappedSource mappedSource = new MappedSourceBuilder().source("src").sourceIdsAdd("srcId").build();
+        String pubMedId = "12345";
+        Set<String> cats = new HashSet<>();
+        cats.add("cat1");cats.add("cat2");
+
+        CommunityAnnotationBuilder communityAnnotationBuilder = new CommunityAnnotationBuilder();
+        communityAnnotationBuilder.comment(comment).proteinOrGene(protOrGene).function(function).disease(disease);
+        CommunityMappedReferenceBuilder builder = new CommunityMappedReferenceBuilder();
+        builder.communityAnnotation(communityAnnotationBuilder.build());
+        builder.uniProtKBAccession(accession).sourcesAdd(mappedSource);
+        builder.pubMedId(pubMedId).sourceCategoriesSet(cats);
+        return builder.build();
+    }
+}
