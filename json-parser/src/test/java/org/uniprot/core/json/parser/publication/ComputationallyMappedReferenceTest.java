cb03bef9aeaf6a5397e7681eedbceb9b45707958
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-12-10 11:29:34 +0000

    add json config for community and computational annotation

---
+++ json-parser/src/test/java/org/uniprot/core/json/parser/publication/ComputationallyMappedReferenceTest.java
@@ -0,0 +1,52 @@
+package org.uniprot.core.json.parser.publication;
+
+import org.junit.jupiter.api.Test;
+import org.uniprot.core.json.parser.ValidateJson;
+import org.uniprot.core.publication.ComputationallyMappedReference;
+import org.uniprot.core.publication.MappedSource;
+import org.uniprot.core.publication.impl.ComputationallyMappedReferenceBuilder;
+import org.uniprot.core.publication.impl.MappedSourceBuilder;
+import org.uniprot.core.uniprotkb.UniProtKBAccession;
+import org.uniprot.core.uniprotkb.impl.UniProtKBAccessionBuilder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author sahmad
+ */
+public class ComputationallyMappedReferenceTest {
+
+    @Test
+    void testSimpleCommunityMappedReference() {
+        ComputationallyMappedReferenceBuilder builder = new ComputationallyMappedReferenceBuilder();
+        ComputationallyMappedReference computationallyMappedReference = builder.build();
+        ValidateJson.verifyJsonRoundTripParser(
+                ComputationallyMappedReferenceJsonConfig.getInstance().getFullObjectMapper(), computationallyMappedReference);
+    }
+
+    @Test
+    void testCompleteCommunityMappedReference() {
+        ComputationallyMappedReference computationallyMappedReference = getCompleteComputationallyMappedReference();
+        ValidateJson.verifyJsonRoundTripParser(
+                ComputationallyMappedReferenceJsonConfig.getInstance().getFullObjectMapper(), computationallyMappedReference);
+        ValidateJson.verifyEmptyFields(computationallyMappedReference);
+    }
+
+    static ComputationallyMappedReference getCompleteComputationallyMappedReference() {
+        String protOrGene = "protOrGene";
+        String function = "function";
+        String disease = "disease";
+        String comment = "comment";
+        UniProtKBAccession accession = new UniProtKBAccessionBuilder("P12345").build();
+        MappedSource mappedSource = new MappedSourceBuilder().source("src").sourceIdsAdd("srcId").build();
+        String pubMedId = "12345";
+        Set<String> cats = new HashSet<>();
+        cats.add("cat1");cats.add("cat2");
+        ComputationallyMappedReferenceBuilder builder = new ComputationallyMappedReferenceBuilder();
+        builder.annotation("annotation");
+        builder.uniProtKBAccession(accession).sourcesAdd(mappedSource);
+        builder.pubMedId(pubMedId).sourceCategoriesSet(cats);
+        return builder.build();
+    }
+}
