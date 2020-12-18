79e7ccde971c6d14c5f1c3481d16788fc773906a
Shadab Ahmad <sahmad@ebi.ac.uk>
2020-12-18 16:06:41 +0000

    add more test cases

--- core-domain/src/test/java/org/uniprot/core/publication/impl/MappedPublicationsBuilderTest.java
+++ core-domain/src/test/java/org/uniprot/core/publication/impl/MappedPublicationsBuilderTest.java
@@ -1,9 +1,12 @@
 package org.uniprot.core.publication.impl;
 
 import org.junit.jupiter.api.Test;
+import org.uniprot.core.publication.CommunityMappedReference;
 import org.uniprot.core.publication.ComputationallyMappedReference;
 import org.uniprot.core.publication.MappedPublications;
 import org.uniprot.core.publication.UniProtKBMappedReference;
+
+import java.util.Arrays;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -11,6 +14,7 @@ import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author sahmad
@@ -65,5 +69,53 @@ public class MappedPublicationsBuilderTest {
         assertThat(mappedPubs.getReviewedMappedReference(), is(nullValue()));
         assertThat(mappedPubs.getUnreviewedMappedReference(), is(nullValue()));
         assertThat(mappedPubs.getCommunityMappedReferences(), is(empty()));
+    }
+
+    @Test
+    void canSetComputationalMappedReference() {
+        ComputationallyMappedReference reference1 = new ComputationallyMappedReferenceImpl();
+        ComputationallyMappedReference reference2 = new ComputationallyMappedReferenceImpl();
+        MappedPublicationsBuilder builder = new MappedPublicationsBuilder();
+        builder.computationalMappedReferencesSet(Arrays.asList(reference1, reference2));
+        MappedPublications mappedPubs = builder.build();
+
+        assertThat(mappedPubs.getComputationalMappedReferences(), hasSize(2));
+        assertThat(mappedPubs.getReviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getUnreviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getCommunityMappedReferences(), is(empty()));
+    }
+
+    @Test
+    void canAddCommunityMappedReference() {
+        CommunityMappedReference reference = new CommunityMappedReferenceImpl();
+        MappedPublicationsBuilder builder = new MappedPublicationsBuilder();
+        builder.communityMappedReferencesAdd(reference);
+        MappedPublications mappedPubs = builder.build();
+
+        assertThat(mappedPubs.getCommunityMappedReferences(), hasSize(1));
+        assertThat(mappedPubs.getReviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getUnreviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getComputationalMappedReferences(), is(empty()));
+    }
+
+    @Test
+    void canSetCommunityMappedReference() {
+        CommunityMappedReference reference1 = new CommunityMappedReferenceImpl();
+        CommunityMappedReference reference2 = new CommunityMappedReferenceImpl();
+        MappedPublicationsBuilder builder = new MappedPublicationsBuilder();
+        builder.communityMappedReferencesSet(Arrays.asList(reference1, reference2));
+        MappedPublications mappedPubs = builder.build();
+
+        assertThat(mappedPubs.getCommunityMappedReferences(), hasSize(2));
+        assertThat(mappedPubs.getReviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getUnreviewedMappedReference(), is(nullValue()));
+        assertThat(mappedPubs.getComputationalMappedReferences(), is(empty()));
+    }
+
+    @Test
+    void canCreateBuilderFromInstance() {
+        MappedPublications obj = new MappedPublicationsBuilder().build();
+        MappedPublicationsBuilder builder = MappedPublicationsBuilder.from(obj);
+        assertNotNull(builder);
     }
 }
