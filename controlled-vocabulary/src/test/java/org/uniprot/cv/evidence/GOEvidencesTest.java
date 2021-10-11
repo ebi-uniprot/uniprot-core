fd82a3b2d2ce248b907158a05874c6fb9a5fddda
LeonardoGonzales <lgonzales@ebi.ac.uk>
2021-10-11 14:32:04 +0100

    fix ECO code changes

--- controlled-vocabulary/src/test/java/org/uniprot/cv/evidence/GOEvidencesTest.java
+++ controlled-vocabulary/src/test/java/org/uniprot/cv/evidence/GOEvidencesTest.java
@@ -19,21 +19,21 @@ class GOEvidencesTest {
     @Test
     void testConvertGAFToECODefault() {
         // IBA	Default	ECO:0000318
-        Optional<String> eco = GOEvidences.INSTANCE.convertGAFToECO("IEA");
-        assertEquals("ECO:0007669", eco.orElse(WRONG));
+        Optional<String> eco = GOEvidences.INSTANCE.convertGAFToECO("IBA");
+        assertEquals("ECO:0000318", eco.orElse(WRONG));
     }
 
     @Test
     void testConvertGAFToECOStringString() {
-        // IEA	GO_REF:0000037	ECO:0000322
+        // IEA	GO_REF:0000037	ECO:0007669
         Optional<String> eco = GOEvidences.INSTANCE.convertGAFToECO("IEA", "GO_REF:0000037");
-        assertEquals("ECO:0000322", eco.orElse(WRONG));
+        assertEquals("ECO:0007669", eco.orElse(WRONG));
     }
 
     @Test
     void testConvertECOToGAF() {
-        Optional<String> gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000322");
-        assertEquals("IEA", gaf.orElse(WRONG));
+        Optional<String> gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000269");
+        assertEquals("EXP", gaf.orElse(WRONG));
 
         gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0007669");
         assertEquals("IEA", gaf.orElse(WRONG));
