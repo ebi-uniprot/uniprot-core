9d81116bd55996090056a9ffcd122aa323ea7b4b
Shadab Ahmad <sahmad@ebi.ac.uk>
2021-12-12 19:55:15 +0000

    fix test

--- controlled-vocabulary/src/test/java/org/uniprot/cv/evidence/GOEvidencesTest.java
+++ controlled-vocabulary/src/test/java/org/uniprot/cv/evidence/GOEvidencesTest.java
@@ -27,12 +27,15 @@ class GOEvidencesTest {
     void testConvertGAFToECOStringString() {
         // IEA	GO_REF:0000037	ECO:0000322
         Optional<String> eco = GOEvidences.INSTANCE.convertGAFToECO("IEA", "GO_REF:0000037");
-        assertEquals("ECO:0000501", eco.orElse(WRONG));
+        assertEquals("ECO:0000322", eco.orElse(WRONG));
     }
 
     @Test
     void testConvertECOToGAF() {
-        Optional<String> gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000501");
+        Optional<String> gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000322");
+        assertEquals("IEA", gaf.orElse(WRONG));
+
+        gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000501");
         assertEquals("IEA", gaf.orElse(WRONG));
 
         gaf = GOEvidences.INSTANCE.convertECOToGAF("ECO:0000318");
