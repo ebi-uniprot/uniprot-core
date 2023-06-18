75e292fb8d57b6da25e98cac399a39aa4119ce12
ktibrahim <ibrahim@ebi.ac.uk>
2023-06-18 13:56:21 +0100

    Exception handling refined for Http client get utility method

--- core-common/src/main/java/org/uniprot/core/util/Utils.java
+++ core-common/src/main/java/org/uniprot/core/util/Utils.java
@@ -1,6 +1,8 @@
 package org.uniprot.core.util;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -185,16 +187,25 @@ public class Utils {
         }
     }
 
-    public static String httpGetRequest(String url) throws Exception {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request =
-                HttpRequest.newBuilder()
-                        .uri(new URI(url))
-                        .version(HttpClient.Version.HTTP_1_1)
-                        .headers("Content-Type", "application/json;charset=UTF-8")
-                        .GET()
-                        .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+    public static String httpGetRequest(String url) {
+        HttpResponse<String> response;
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request =
+                    HttpRequest.newBuilder()
+                            .uri(new URI(url))
+                            .version(HttpClient.Version.HTTP_1_1)
+                            .headers("Content-Type", "application/json;charset=UTF-8")
+                            .GET()
+                            .build();
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
         return response.body();
     }
