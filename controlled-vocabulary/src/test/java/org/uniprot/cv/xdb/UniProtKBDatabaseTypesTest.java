d8ca7abed11b15d50798751e1e6b8f6f46983af6
LeonardoGonzales <lgonzales@ebi.ac.uk>
2023-07-12 09:21:58 +0100

    Revert "Added GeneDB temporarily for 2023_04 release"
    This reverts commit ed39c53d630d283f0f09995832f2f107582c11f4.

--- controlled-vocabulary/src/test/java/org/uniprot/cv/xdb/UniProtKBDatabaseTypesTest.java
+++ controlled-vocabulary/src/test/java/org/uniprot/cv/xdb/UniProtKBDatabaseTypesTest.java
@@ -311,7 +311,7 @@ class UniProtKBDatabaseTypesTest {
                 4);
         verifyGroupSize(
                 UniProtDatabaseTypes.INSTANCE.getDBTypesByCategory(GENOME_ANNOTATION_DATABASES),
-                16);
+                15);
         verifyGroupSize(
                 UniProtDatabaseTypes.INSTANCE.getDBTypesByCategory(ORGANISM_SPECIFIC_DATABASES),
                 40);
