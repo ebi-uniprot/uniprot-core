762a1968048235bd0ef18417337ca911ba5d9858
Shadab Ahmad <sahmad@ebi.ac.uk>
2023-09-27 17:12:12 +0100

    add internal cross refs for publication

--- controlled-vocabulary/src/test/java/org/uniprot/cv/xdb/CrossReferenceValidatorIT.java
+++ controlled-vocabulary/src/test/java/org/uniprot/cv/xdb/CrossReferenceValidatorIT.java
@@ -69,7 +69,7 @@ class CrossReferenceValidatorIT {
                         opType.getAttributes(),
                         false,
                         null,
-                        null);
+                        null, null);
 
         // validate, the category should mismatch
         List<Pair<String, String>> mismatches = CrossReferenceValidator.validate(actualOpType);
--- core-domain/src/main/java/org/uniprot/core/cv/xdb/UniProtDatabaseDetail.java
+++ core-domain/src/main/java/org/uniprot/core/cv/xdb/UniProtDatabaseDetail.java
@@ -20,6 +20,7 @@ public class UniProtDatabaseDetail implements Serializable {
     private boolean implicit = false;
     private String linkedReason = null;
     private String idMappingName;
+    private String type;
 
     UniProtDatabaseDetail() {
         this.attributes = new ArrayList<>();
@@ -35,6 +36,18 @@ public class UniProtDatabaseDetail implements Serializable {
             boolean implicit,
             String linkedReason,
             String idMappingName) {
+        this(name, displayName, category, uriLink, attributes, implicit, linkedReason, idMappingName, null);
+    }
+    public UniProtDatabaseDetail(
+            String name,
+            String displayName,
+            UniProtDatabaseCategory category,
+            String uriLink,
+            List<UniProtDatabaseAttribute> attributes,
+            boolean implicit,
+            String linkedReason,
+            String idMappingName,
+            String type) {
         super();
         this.name = name;
         this.displayName = displayName;
@@ -47,6 +60,7 @@ public class UniProtDatabaseDetail implements Serializable {
         if ((attributes != null) && !attributes.isEmpty()) this.attributes.addAll(attributes);
         else this.attributes.add(DEFAULT_ATTRIBUTE);
         this.idMappingName = idMappingName;
+        this.type = type;
     }
 
     public String getName() {
@@ -81,6 +95,10 @@ public class UniProtDatabaseDetail implements Serializable {
         return idMappingName;
     }
 
+    public String getType(){
+        return this.type;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hash(
@@ -91,7 +109,8 @@ public class UniProtDatabaseDetail implements Serializable {
                 this.uriLink,
                 this.implicit,
                 this.linkedReason,
-                this.idMappingName);
+                this.idMappingName,
+                this.type);
     }
 
     @Override
@@ -109,6 +128,7 @@ public class UniProtDatabaseDetail implements Serializable {
                 && Objects.equals(this.uriLink, other.uriLink)
                 && Objects.equals(this.implicit, other.implicit)
                 && Objects.equals(this.linkedReason, other.linkedReason)
-                && Objects.equals(this.idMappingName, other.idMappingName);
+                && Objects.equals(this.idMappingName, other.idMappingName)
+                && Objects.equals(this.type, other.type);
     }
 }
