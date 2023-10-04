f99e4f116d0df52ae4b1e64e888763c2f5ac6b95
supun <supun@ebi.ac.uk>
2023-10-04 16:03:10 +0100

    add defualt constructor to proteome stats

--- core-domain/src/main/java/org/uniprot/core/proteome/impl/ProteomeStatisticsImpl.java
+++ core-domain/src/main/java/org/uniprot/core/proteome/impl/ProteomeStatisticsImpl.java
@@ -1,17 +1,22 @@
 package org.uniprot.core.proteome.impl;
 
-import java.util.Objects;
-
 import org.uniprot.core.impl.StatisticsImpl;
 import org.uniprot.core.proteome.ProteomeStatistics;
 
+import java.util.Objects;
+
 public class ProteomeStatisticsImpl extends StatisticsImpl implements ProteomeStatistics {
+    private static final long serialVersionUID = -8585233468042758658L;
     private final long isoformProteinCount;
 
     ProteomeStatisticsImpl(
             long reviewedProteinCount, long unreviewedProteinCount, long isoformProteinCount) {
         super(reviewedProteinCount, unreviewedProteinCount);
         this.isoformProteinCount = isoformProteinCount;
+    }
+
+    ProteomeStatisticsImpl() {
+        this(0, 0,0);
     }
 
     @Override
