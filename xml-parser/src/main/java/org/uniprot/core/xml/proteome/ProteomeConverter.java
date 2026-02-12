2658100ed9aee41bc9e746fe7b2f891bae790219
Supun Wijerathne <supun@ebi.ac.uk>
2026-02-12 14:17:33 +0000

    make get proteome type of proteome converter static

--- xml-parser/src/main/java/org/uniprot/core/xml/proteome/ProteomeConverter.java
+++ xml-parser/src/main/java/org/uniprot/core/xml/proteome/ProteomeConverter.java
@@ -188,7 +188,7 @@ public class ProteomeConverter implements Converter<Proteome, ProteomeEntry> {
         }
     }
 
-    private ProteomeType getProteomeType(Proteome t) {
+    private static ProteomeType getProteomeType(Proteome t) {
         ProteomeType proteomeType = ProteomeType.typeOf(t.getProteomeStatus());
         if (SURVEILLANCE.equals(proteomeType)) {
             return EXCLUDED;
